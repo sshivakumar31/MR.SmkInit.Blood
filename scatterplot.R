557b2272f1f8ebd54ca90fb35df71123f0cdeec0
sshivakumar31 <shivashriya@gmail.com>
2023-07-20 15:16:32 -0400

    draft of scatterplot

--- scatterplot.R
+++ scatterplot.R
@@ -5,8 +5,11 @@ read.csv("/Users/shriyasaishivakumar/MR.SmkInit.Blood/DrnksWk.EUR.csv")
 ds$id.exposure <- sub("^[^.]+\\.(.*?)\\..*$", "\\1", ds$id.exposure)
 
 bp <- ggplot(ds, aes(x = b, y = id.exposure)) +
-  geom_point() +
-  labs(title = "DrnksWk EUR -> EUR", y = "Exposure", x = "Effect Size")
+  geom_point(color="blue") +
+  geom_smooth(methods = "lm", se = TRUE) +
+  geom_errorbarh(aes(xmin = b - se, xmax = b + se), height = 0) +
+  labs(title = "DrnksWk EUR -> EUR", y = "Exposure", x = "Effect Size")+
+  theme_minimal()
 
 # Color code y-axis labels
 bp <- bp + theme(axis.text.y = element_text(color = c("red", "blue", 
@@ -16,5 +19,5 @@ bp <- bp + theme(axis.text.y = element_text(color = c("red", "blue",
 bp <- bp + geom_text(data = ds, aes(label = paste("SE =", se)), hjust = 
 -0.2, color = "black")
 
-ggsave("scatterplot1.pdf")
+ggsave("scatterplotdraft.pdf")
 
